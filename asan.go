bda31cd02f587923700661e653639ea0b90fca1c
wbartel <wilhelm.bartel@streamonkey.de>
2024-12-09 11:41:32 +0100

    call GC and add docs for why

--- asan.go
+++ asan.go
@@ -7,8 +7,15 @@ package asanutils
 //
 // void __lsan_do_leak_check(void);
 import "C"
+import "runtime"
 
 // CheckLeaks triggers the `lsan` Leaks Checker, but only when compiled with `asan` support
 func CheckLeaks() {
+	// finalizers are funky, so we need to call the GC twice.
+	// The reason is that a finalizer revives the object and cleans it in the next GC cycle.
+	runtime.GC()
+	runtime.GC()
+
+	runtime.GC() // one more for good measure :)
 	C.__lsan_do_leak_check()
 }
